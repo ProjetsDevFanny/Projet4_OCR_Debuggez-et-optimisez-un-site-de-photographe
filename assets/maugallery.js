89c0592436cadbe265e37b67536eacf38e3eb6ba
Fanny Simon <contact.fanny.simon@gmail.com>
2025-09-15 13:42:16 +0200

    résolution du bug dans la console : conflit aria-hidden=true et focus sur la lightbox

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -1,9 +1,28 @@
-(function($) {
-  $.fn.mauGallery = function(options) {
+// ======================================================================================
+// Fichier maugallery.js : plugin jQuery qui va transformer les images en galerie responsive avec filtre et lightbox.
+
+// Plus précisément, ce code définit une fonction réutilisable (un plugin jQuery) qu’on peut appliquer sur un élément de la page (par exemple ici une galerie d’images), en mélangeant les options par défaut avec celles qui ont été choisies quand on appelle la fonction.
+// ======================================================================================
+
+(function ($) {
+  // ---------------Définition du plugin jQuery "mauGallery"----------------------
+
+  $.fn.mauGallery = function (options) {
+    // Fusion des options par défaut et de celles fournies par l'utilisateur
     var options = $.extend($.fn.mauGallery.defaults, options);
-    var tagsCollection = [];
-    return this.each(function() {
+    // $.extend : fusionne les objets options et defaults:
+    // defaults = les valeurs par défaut données par le plugin (ex: {columns: 3, showTags: true})
+    // options = ce que l’utilisateur a mis (les paramètres donnés depuis script.js)
+
+    var tagsCollection = []; // Tableau pour stocker les tags uniques trouvés
+
+    // ---------------Application du plugin à chaque galerie trouvée :----------------------
+
+    return this.each(function () {
+      // Création du conteneur .row pour ranger les images.
       $.fn.mauGallery.methods.createRowWrapper($(this));
+
+      // Si l’option lightBox est activée, on crée la modale/lightbox
       if (options.lightBox) {
         $.fn.mauGallery.methods.createLightBox(
           $(this),
@@ -11,24 +30,34 @@
           options.navigation
         );
       }
+
+      // Activation des écouteurs d’événements
       $.fn.mauGallery.listeners(options);
 
+      // Pour chaque élément .gallery-item
       $(this)
         .children(".gallery-item")
-        .each(function(index) {
+        .each(function (index) {
+          // Rendre l’image responsive si c’est une balise <img>
           $.fn.mauGallery.methods.responsiveImageItem($(this));
+          // Déplacer l’élément dans la .row créée
           $.fn.mauGallery.methods.moveItemInRowWrapper($(this));
+          // Envelopper l’élément dans une colonne (Bootstrap)
           $.fn.mauGallery.methods.wrapItemInColumn($(this), options.columns);
+
+          // Récupération du tag (catégorie) associé à l’image
           var theTag = $(this).data("gallery-tag");
+          // Si on affiche les tags, qu’il est défini, et qu’il n’est pas déjà dans la liste
           if (
             options.showTags &&
             theTag !== undefined &&
             tagsCollection.indexOf(theTag) === -1
           ) {
-            tagsCollection.push(theTag);
+            tagsCollection.push(theTag); // On ajoute le tag au tableau
           }
         });
 
+      // Si l’option d’affichage des tags est activée, on les affiche
       if (options.showTags) {
         $.fn.mauGallery.methods.showItemTags(
           $(this),
@@ -37,27 +66,39 @@
         );
       }
 
+      // Apparition progressive de la galerie
       $(this).fadeIn(500);
     });
   };
+
+  //---------------Définition des options par défaut du plugin----------------------
+
   $.fn.mauGallery.defaults = {
-    columns: 3,
-    lightBox: true,
-    lightboxId: null,
-    showTags: true,
-    tagsPosition: "bottom",
-    navigation: true
+    columns: 3, // Nombre de colonnes (par défaut 3)
+    lightBox: true, // Active ou non la lightbox
+    lightboxId: null, // ID de la modale (lightbox)
+    showTags: true, // Affiche ou non les tags
+    tagsPosition: "bottom", // Position des tags (haut ou bas)
+    navigation: true, // Boutons précédent/suivant dans la lightbox
   };
-  $.fn.mauGallery.listeners = function(options) {
-    $(".gallery-item").on("click", function() {
+
+  // ---------------Configuration des écouteurs d’événements----------------------
+
+  $.fn.mauGallery.listeners = function (options) {
+    // Quand on clique sur une image de la galerie
+    $(".gallery-item").on("click", function () {
       if (options.lightBox && $(this).prop("tagName") === "IMG") {
+        // Ouvre la lightbox avec l’image cliquée
         $.fn.mauGallery.methods.openLightBox($(this), options.lightboxId);
       } else {
         return;
       }
     });
 
+    // Filtrage par tag quand on clique sur un bouton de navigation
     $(".gallery").on("click", ".nav-link", $.fn.mauGallery.methods.filterByTag);
+
+    // Navigation (image précédente/suivante dans la lightbox)
     $(".gallery").on("click", ".mg-prev", () =>
       $.fn.mauGallery.methods.prevImage(options.lightboxId)
     );
@@ -65,23 +106,30 @@
       $.fn.mauGallery.methods.nextImage(options.lightboxId)
     );
   };
+
+  // ---------------Méthodes principales du plugin----------------------
+
   $.fn.mauGallery.methods = {
+    // ---------------Création d'un conteneur .row pour organiser les images, seulement s’il n’existe pas déjà-----
+
     createRowWrapper(element) {
-      if (
-        !element
-          .children()
-          .first()
-          .hasClass("row")
-      ) {
+      if (!element.children().first().hasClass("row")) {
         element.append('<div class="gallery-items-row row"></div>');
       }
     },
+
+    // ---------------Enveloppe un élément dans une colonne responsive----------------------
+
+    // On enveloppe chaque image dans une colonne responsive (Bootstrap), en fonction du nombre de colonnes ou d’un objet avec les tailles (xs, sm, md, etc.).
+
     wrapItemInColumn(element, columns) {
       if (columns.constructor === Number) {
+        // Cas simple : nombre fixe de colonnes
         element.wrap(
           `<div class='item-column mb-4 col-${Math.ceil(12 / columns)}'></div>`
         );
       } else if (columns.constructor === Object) {
+        // Cas avancé : colonnes définies par device (xs, sm, md, lg, xl)
         var columnClasses = "";
         if (columns.xs) {
           columnClasses += ` col-${Math.ceil(12 / columns.xs)}`;
@@ -100,101 +148,151 @@
         }
         element.wrap(`<div class='item-column mb-4${columnClasses}'></div>`);
       } else {
+        // Erreur si le format des colonnes n’est pas valide
         console.error(
           `Columns should be defined as numbers or objects. ${typeof columns} is not supported.`
         );
       }
     },
+
+    // ---------------Déplacement de chaque élément dans la .row créée----------------------
+
     moveItemInRowWrapper(element) {
       element.appendTo(".gallery-items-row");
     },
+
+    // ---------------Rend une image responsive si c’est une balise <img> et en leur ajoutant la classe Bootstrap .img-fluid----------------------
+
     responsiveImageItem(element) {
       if (element.prop("tagName") === "IMG") {
         element.addClass("img-fluid");
       }
     },
+
+    // ---------------Ouvre la lightbox et on affiche l’image cliquée dedans----------------------
+
+    // openLightBox(element, lightboxId) {
+    //   $(`#${lightboxId}`)
+    //     .find(".lightboxImage")
+    //     .attr("src", element.attr("src"));
+    //   $(`#${lightboxId}`).modal("toggle");
+    // },
+
+    // -----------Correction du bug :conflit focus sur la lightBox et le focus sur la première image de la galerie.----------------------
+
     openLightBox(element, lightboxId) {
-      $(`#${lightboxId}`)
-        .find(".lightboxImage")
-        .attr("src", element.attr("src"));
-      $(`#${lightboxId}`).modal("toggle");
+      const modal = $(`#${lightboxId}`);
+      modal.find(".lightboxImage").attr("src", element.attr("src"));
+      modal.attr("aria-hidden", "false"); // modale ouverte, on désactive l'attribut aria-hidden pour que le lecteur de screen reader puisse la voir.
+      modal.modal("toggle");
     },
+
+    // On gère l'événement de fermeture pour remettre aria-hidden à true:
+    // Bootstrap déclenche l’événement hidden.bs.modal quand la modale se ferme, donc on peut ajouter :
+    closeLightBox(lightboxId) {
+      const modal = $(`#${lightboxId}`);
+      modal.on("hidden.bs.modal", function () {
+        $(this).attr("aria-hidden", "true"); // modale fermée ->  Cela indique aux technologies d’assistance que la modal est maintenant cachée et ne doit plus être lue ou focusée
+      });
+    },
+
+    // ---------------On gère l’image précédente dans la lightbox----------------------
+
     prevImage() {
       let activeImage = null;
-      $("img.gallery-item").each(function() {
+      // On cherche l’image active
+      $("img.gallery-item").each(function () {
         if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
           activeImage = $(this);
         }
       });
+
+      // On récupère la liste des images selon le tag actif (filtrage).
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
-      let imagesCollection = [];
+      let imagesCollection = []; // On crée un tableau pour stocker les images visibles.
+
+      // Création de la collection d’images visibles
       if (activeTag === "all") {
-        $(".item-column").each(function() {
+        $(".item-column").each(function () {
           if ($(this).children("img").length) {
             imagesCollection.push($(this).children("img"));
           }
         });
       } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
+        $(".item-column").each(function () {
+          if ($(this).children("img").data("gallery-tag") === activeTag) {
             imagesCollection.push($(this).children("img"));
           }
         });
       }
+
       let index = 0,
         next = null;
 
-      $(imagesCollection).each(function(i) {
+      // Trouver l’index de l’image active
+      $(imagesCollection).each(function (i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
+          index = i;
+          console.log(index); // pour vérifier que l'index est correct
         }
       });
+
+      // Image précédente (ou dernière si aucune trouvée)
       next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+        // imagesCollection[index] || → Gardait la même image, donc rien ne changeait quand on cliquait.
+        imagesCollection[index - 1] || // Correction : on soustrait 1 à l'index pour afficher l'image précédente
+        imagesCollection[imagesCollection.length - 1]; // Si aucune image précédente trouvée, on affiche la dernière image (pour boucler).
+      $(".lightboxImage").attr("src", $(next).attr("src")); // On change l'attribut src de l'image affichée dans la lightbox et on lui met celui de la nouvelle image.
     },
+
+    // ---------------Gestion de l'image suivante dans la lightbox----------------------
+
     nextImage() {
       let activeImage = null;
-      $("img.gallery-item").each(function() {
+      // On cherche l’image active
+      $("img.gallery-item").each(function () {
         if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
           activeImage = $(this);
         }
       });
+
+      // On récupère la liste des images selon le tag actif (filtrage).
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
-      let imagesCollection = [];
+      let imagesCollection = []; // On crée un tableau pour stocker les images visibles.
+
+      // Création de la collection d’images visibles
       if (activeTag === "all") {
-        $(".item-column").each(function() {
+        $(".item-column").each(function () {
           if ($(this).children("img").length) {
             imagesCollection.push($(this).children("img"));
           }
         });
       } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
+        $(".item-column").each(function () {
+          if ($(this).children("img").data("gallery-tag") === activeTag) {
             imagesCollection.push($(this).children("img"));
           }
         });
       }
+
       let index = 0,
         next = null;
 
-      $(imagesCollection).each(function(i) {
+      $(imagesCollection).each(function (i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+
+      // Image suivante (ou première si aucune trouvée)
+
+      // next = imagesCollection[index] || imagesCollection[0]; → Gardait la même image, donc rien ne changeait quand on cliquait.
+      next = imagesCollection[index + 1] || imagesCollection[0]; // Correction : on ajoute 1 à l'index pour afficher l'image suivante
+      $(".lightboxImage").attr("src", $(next).attr("src")); // On change l'attribut src de l'image affichée dans la lightbox et on lui met celui de la nouvelle image.
     },
+
+    // ---------------Création de la lightbox (modale Bootstrap) avec son contenu : image + boutons précédent/suivant si navigation est activé----------------------
+
     createLightBox(gallery, lightboxId, navigation) {
       gallery.append(`<div class="modal fade" id="${
         lightboxId ? lightboxId : "galleryLightbox"
@@ -207,7 +305,7 @@
                                 ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
-                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
+                            <img class="lightboxImage img-fluid" alt="Contenu de l\'image affichée dans la modale au clique"/>
                             ${
                               navigation
                                 ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
@@ -218,10 +316,13 @@
                 </div>
             </div>`);
     },
+
+    // ---------------Affiche les tags de filtrage (catégories)----------------------
+
     showItemTags(gallery, position, tags) {
       var tagItems =
         '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
-      $.each(tags, function(index, value) {
+      $.each(tags, function (index, value) {
         tagItems += `<li class="nav-item active">
                 <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
       });
@@ -235,29 +336,29 @@
         console.error(`Unknown tags position: ${position}`);
       }
     },
+
+    // ---------------Filtre les images par tag----------------------
+
     filterByTag() {
       if ($(this).hasClass("active-tag")) {
         return;
       }
+
+      // Mise à jour du tag actif
       $(".active-tag").removeClass("active active-tag");
       $(this).addClass("active-tag");
 
       var tag = $(this).data("images-toggle");
 
-      $(".gallery-item").each(function() {
-        $(this)
-          .parents(".item-column")
-          .hide();
+      // Affiche/masque les images selon le tag sélectionné
+      $(".gallery-item").each(function () {
+        $(this).parents(".item-column").hide();
         if (tag === "all") {
-          $(this)
-            .parents(".item-column")
-            .show(300);
+          $(this).parents(".item-column").show(300);
         } else if ($(this).data("gallery-tag") === tag) {
-          $(this)
-            .parents(".item-column")
-            .show(300);
+          $(this).parents(".item-column").show(300);
         }
       });
-    }
+    },
   };
-})(jQuery);
+})(jQuery); // Fin de la fonction auto-exécutée
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,28 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+// ======================================================================================
+// Fichier script.js : script qui va charger le plugin maugallery.js et appliquer la fonction .mauGallery() à l'élément .gallery.
+
+// Quand la page est prête ($(document).ready), on applique la fonction .mauGallery() à l'élément .gallery.
+// Cette fonction est définie dans le fichier maugallery.js.
+
+//========================================================================================
+
+// Quand le DOM (page HTML) est entièrement chargé
+$(document).ready(function () {
+  // On initialise la galerie sur l'élément qui a la classe .gallery
+  $(".gallery").mauGallery({
+    // Définition du nombre de colonnes selon la taille d'écran (Bootstrap grid)
+    columns: {
+      xs: 1, // petits écrans (téléphones)
+      sm: 2, // écrans un peu plus grands
+      md: 3, // écrans moyens
+      lg: 3, // grands écrans
+      xl: 3, // très grands écrans
+    },
+    // ... options ...
+    lightBox: true, // active l'agrandissement des images (lightbox)
+    lightboxId: "myAwesomeLightbox", // id du modal qui sera créé pour le lightbox
+    showTags: true, // affiche les filtres (tags) au-dessus ou en-dessous
+    tagsPosition: "top", // position de la barre de tags ("top" ou "bottom")
+    // navigation: true, // ou false pour désactiver les boutons précédent/suivant dans la lightbox = valeur par défaut du plugin est "true", donc on peut la supprimer.
+  });
 });
